7dc1bff0f7a510bdc492fd14d565279013ae0a3c
helmai1 <helmiefendi64@gmail.com>
2023-08-14 23:46:58 +0700

    add index & function pangkat 2

---
+++ script.js
@@ -0,0 +1,50 @@
+const calculator = `
+Pilih Menu:
+1. Penjumlahan
+2. Penguranngan
+3. Pembagian
+4. Perkalian
+5. Pangkat 2
+6. Akar Pangkat 2
+7. Akar Pangkat 3`;
+const menu = prompt(calculator);
+console.log(menu);
+
+switch (menu) {
+  case '1':
+    penjumlahan();
+    break;
+  case '2':
+    pengurangan();
+    break;
+  case '3':
+    pembagian();
+    break;
+  case '4':
+    perkalian();
+    break;
+  case '5':
+    pangkat2();
+    break;
+  case '6':
+    akarPangkat2();
+    break;
+  case '7':
+    akarPangkat3();
+    break;
+  default:
+    alert('Masukan anda tidak terdapat pada menu');
+}
+
+function penjumlahan() {}
+function pengurangan() {}
+function pembagian() {}
+function perkalian() {}
+// add by Helmi Effendi
+function pangkat2() {
+  const num = prompt('Input number for squared : ');
+  const result = parseInt(num) * parseInt(num);
+  alert(`${num} Squared is ${result}`);
+}
+function akarPangkat2() {}
+function akarPangkat3() {}
