8a5db8d7ba89b8480ba4d028cd0bab2ba08a664e
Muchamad Choirul Anwar <mcnwr76@192.168.1.4>
2023-08-14 22:02:58 +0700

    add: pengurangan & akar ^3

---
+++ akar_pangakat_tiga.js
@@ -0,0 +1,8 @@
+function akarPangkatTiga(a) {
+  return a ** 3;
+}
+
+console.log(akarPangkatTiga(10)); //1000
+console.log(akarPangkatTiga(3)); //27
+console.log(akarPangkatTiga(5)); //125
+console.log(akarPangkatTiga(9)); //729
